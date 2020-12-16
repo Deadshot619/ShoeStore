0986d78763f25e0f34f5d3ddda0e3927e9269325
Deadshot <pankaj.makhija26@gmail.com>
2020-12-16 23:01:55 +0530

    - Add Login page as fragment - Add few dimensions & strings - Add Navigation file

---
+++ starter/app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
@@ -0,0 +1,34 @@
+package com.udacity.shoestore.ui.login
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LoginFragment()
+    }
+
+    private val mBinding by lazy { FragmentLoginBinding.inflate(layoutInflater) }
+    private val viewModel: LoginViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return mBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+}
